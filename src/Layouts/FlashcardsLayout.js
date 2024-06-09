3bf47f22d08ff67ba4fb8c4bff0e686497f61165
Denys <gusardavydov2004@gmail.com>
2024-06-09 22:53:52 +0300

    made build, made fun

--- src/Layouts/FlashcardsLayout.js
+++ src/Layouts/FlashcardsLayout.js
@@ -1,16 +1,7 @@
 import React from "react";
-import { Breadcrumb, Layout, Menu, Space, theme } from 'antd';
-import { MenuItemType } from "antd/es/menu/interface";
-import { Header } from "antd/es/layout/layout";
+import { Layout } from 'antd';
 import './HomeLayout.css';
-import MainPage from "../FlashCards/MainPage"; // Імпортуйте CSS-файл
-
-const items: Array<MenuItemType> = [
-    { key: 1, label: "Home" },
-    { key: 2, label: "Flash Cards"},
-    { key: 3, label: "Speed Typing"},
-    { key: 4, label: "About us"}
-];
+import MainPage from "../FlashCards/MainPage";
 
 function FlashcardsLayout() {
     return (
--- src/Layouts/SpeedTypingLayout.js
+++ src/Layouts/SpeedTypingLayout.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { Breadcrumb, Layout, Menu, Space, theme } from 'antd';
-import { MenuItemType } from "antd/es/menu/interface";
-import { Header } from "antd/es/layout/layout";
-import './HomeLayout.css'; // Імпортуйте CSS-файл
+import { Layout} from 'antd';
+import './HomeLayout.css';
 import SpeedTyping from "../SpeedTyping/SpeedTyping";
 
 function SpeedTypingLayout() {
